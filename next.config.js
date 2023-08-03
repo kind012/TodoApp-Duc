5aedbb4b85f0eab2c1b12b6e2ebbd904f520a8ff
kind012 <tranthienduc1001@gmal.com>
2023-08-03 15:33:41 +0700

    add config nextjs .env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const webpack = require("webpack");
+const { parsed: myEnv } = require("dotenv").config({
+  path: `${process.env.MONGODB_URL_CONNECT}`,
+});
+const nextConfig = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
